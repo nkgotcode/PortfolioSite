29c8e3d49de347a43f7b1f2e8bba8b749c7c713d
Khanh Le <58643031+nkgotcode@users.noreply.github.com>
2023-06-24 16:37:31 +0700

    ver 1.1.5b1

--- src/lib/HomeBackground.svelte
+++ src/lib/HomeBackground.svelte
@@ -4,10 +4,12 @@
 	import { backOut, quartInOut } from 'svelte/easing';
 	import { menu } from '$lib/MenuLoad.js';
 	import Image from 'svelte-image';
+	import { lazyLoad } from '$lib/LazyLoad.js';
 
 	let src = '/@312 no text.webp';
 	let onLoad = false;
 	let img_load = false;
+	const imgUrl = new URL(src, import.meta.url).href;
 
 	onMount(() => {
 		setTimeout(() => (onLoad = true));
@@ -17,22 +19,46 @@
 		menu.set(true);
 		img_load = true;
 	}
+
+	// grab some place holder images
+	async function fetchData() {
+		const res = await fetch(imgUrl);
+		const data = await res.json();
+
+		if (res.ok) {
+			return data;
+		} else {
+			throw new Error(data);
+		}
+	}
 </script>
 
 <div class="container">
-<div
-	in:fly|global={{ y: 1000, duration: 800, easing: quartInOut }}
-	out:fade|global={{ duration: 500, easing: backOut }}
-	on:introend={loadMenu}
->
-	<Image {src} />
-</div>
-{#if img_load}
-	<h1 in:fly|global={{ y: 50, duration: 700 }} out:fly|global={{ y: 700, duration: 1000, opacity: 0.2 }}>
-		Hello there, itsnk
-	</h1>
-{/if}
-<slot />
+	<div
+		in:fly|global={{ y: 1000, duration: 800, easing: quartInOut }}
+		out:fade|global={{ duration: 500, easing: backOut }}
+		on:introend={loadMenu}
+	>
+		<img {src} loading="lazy" alt="img" />
+		<!-- {#await fetchData()} -->
+		<!-- <Image {src} /> -->
+		<!-- <img {src} alt="placeholder" /> -->
+		<!-- <p>...Loading...</p>
+		{:then image}
+			<img use:lazyLoad={imgUrl} />
+		{:catch e}
+			<p style="color: red">{e.message}</p>
+		{/await} -->
+	</div>
+	{#if img_load}
+		<h1
+			in:fly|global={{ y: 50, duration: 700 }}
+			out:fly|global={{ y: 700, duration: 1000, opacity: 0.2 }}
+		>
+			Hello there, itsnk
+		</h1>
+	{/if}
+	<slot />
 </div>
 
 <style>
@@ -41,7 +67,7 @@
 		display: block;
 		max-width: 100%;
 		height: auto;
-		width: auto;
+		width: 100%;
 	}
 	h1 {
 		display: block;
